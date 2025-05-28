7f39ab6abfa18c61c3181295e99bcaaf14049c3f
Kaminski Aleksandr <testEmail@gmail.com>
2025-05-28 19:22:22 +0300

    добавил закрытие потока

--- src/main/java/org/Kaminski/Main.java
+++ src/main/java/org/Kaminski/Main.java
@@ -1,9 +1,14 @@
 package org.Kaminski;
 
 import org.Kaminski.userservice.ui.UserConsoleApp;
+import org.Kaminski.userservice.util.HibernateUtil;
 
 public class Main {
     public static void main(String[] args) {
-        UserConsoleApp.start();
+        try{
+            UserConsoleApp.start();
+        } finally {
+            HibernateUtil.shutdown();
+        }
     }
 }
